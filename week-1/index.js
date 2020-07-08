4a0719d45cc10187751af095b803129e3c95c200
michaelcase1 <65096782+michaelcase1@users.noreply.github.com>
2020-07-08 15:07:38 -0700

    init

---
+++ week-1/index.js
@@ -0,0 +1,9 @@
+var shopper = {
+  myName: "michael",
+  myAge: 24,
+  isAlive: true,
+  fullName: function(){
+      return this.myName + " " + this.myAge;
+  }
+};
+console.log(shopper.myName.length);
