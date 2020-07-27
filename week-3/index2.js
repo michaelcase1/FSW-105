6f5fc1e129dc5aaffe770ff6ed4e7347fc8a30d4
michaelcase1 <65096782+michaelcase1@users.noreply.github.com>
2020-07-26 17:42:30 -0700

    init

---
+++ week-3/index2.js
@@ -0,0 +1,25 @@
+
+function sum(num1, num2){
+    return num1 + num2
+}
+
+var result = sum(2,19)
+console.log(result)
+
+function get_max(num1, num2, num3){
+   
+    return max
+}
+
+var max = Math.max(11, 12, 13)
+console.log(max)
+
+
+function isEven(n) {
+    return (n % 2) == 0;
+  }
+ var n = (2)
+ console.log(isEven)
+ 
+ 
+ 
