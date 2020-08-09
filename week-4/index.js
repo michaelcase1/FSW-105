77d8db13b8baa3ac05eb1df12572e264b0711c90
michaelcase1 <65096782+michaelcase1@users.noreply.github.com>
2020-08-08 17:08:44 -0700

    init

--- week-4/index.js
+++ week-4/index.js
@@ -1,10 +1,39 @@
-var fruit = ["banana", "apple", "orange","watermelon"];
-   
-var vegetables = ["carrot", "tomato","pepper","lettuce"];
-fruit.shift()
-vegetables.pop()
-console.log(vegetables)
+var fruit = ["banana","apple","orange","watermelon"];
 
-console.log(fruit)
+var vegetables = ["carrot","tomato","pepper","lettuce"];
+//console.log(fruit)
+//console.log(vegetables)
+
+vegetables.pop()
+fruit.shift()
+
+///console.log(vegetables)
+///console.log(fruit)
+
 var orangeIndex = fruit.indexOf("orange")
-console.log(orangeIndex)
+//console.log(orangeIndex)
+
+fruit.push(orangeIndex)
+
+
+//console.log(fruit)
+//console.log (vegetables)
+
+var foods = vegetables.concat(fruit)
+
+//console.log(foods)
+foods.splice(4,2)
+//console.log(foods)
+reversedArr = foods.reverse()
+//console.log(reversedArr)
+ var strArr = reversedArr.join()
+ console.log(strArr)
+
+ console.log("**********************************************")
+
+ var name = "luke"
+
+ var upperCasedName = name.toUpperCase()
+ console.log(upperCasedName)
+
+ console.log( Math.floor(50))
